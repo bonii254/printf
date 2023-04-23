a8dfa8728e99baf14d79b6573b1f1abd19300f83
Reuben Mathenge <reubirush7@gmail.com>
2023-04-23 12:40:05 -0700

    display_opt.c

---
+++ display_opt.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * output_opt - check the datatype output option.
+  * @format: string input.
+  * @index: format index to determine output.
+  * @arg_list: list of argument to be printed.
+  *
+  * Return: number of characters printed.
+  */
+int output_opt(const char *format, int *index, va_list arg_list)
+{
+	int i = 0;
+
+	p_opt opt[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		/*
+		 * {'i', print_int}, {'d', print_int}, {'b', print_binary},
+		 * {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		 * {'X', print_hexa_upper}
+		 */ {'\0', NULL}
+	};
+
+	while (opt[i].str != '\0')
+	{
+		if (format[*index] == opt[i].str)
+			return (opt[i].func(arg_list));
+	i++;
+	}
+	return (0);
+}
